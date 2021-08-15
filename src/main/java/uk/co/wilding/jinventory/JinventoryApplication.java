924f2e4a428857dea5bfa18a7ce3d16dfb6800fa
Chris Wilding <hello@chriswilding.co.uk>
2021-08-15 17:36:48 +0100

    Initialise empty project

---
+++ src/main/java/uk/co/wilding/jinventory/JinventoryApplication.java
@@ -0,0 +1,12 @@
+package uk.co.wilding.jinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinventoryApplication.class, args);
+    }
+}
---
+++ src/test/java/uk/co/wilding/jinventory/JinventoryApplicationTests.java
@@ -0,0 +1,12 @@
+package uk.co.wilding.jinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
