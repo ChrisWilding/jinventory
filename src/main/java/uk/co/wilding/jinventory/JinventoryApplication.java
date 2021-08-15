471e7276c4a3f267d444407878a32dca85b2fadf
Chris Wilding <hello@chriswilding.co.uk>
2021-08-15 21:06:26 +0100

    Add log

--- src/main/java/uk/co/wilding/jinventory/JinventoryApplication.java
+++ src/main/java/uk/co/wilding/jinventory/JinventoryApplication.java
@@ -1,12 +1,15 @@
 package uk.co.wilding.jinventory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JinventoryApplication {
 
     public static void main(String[] args) {
+        log.info("test deploy to heroku");
         SpringApplication.run(JinventoryApplication.class, args);
     }
 }
